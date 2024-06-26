f5096e1b8e2d23f49bda450f9a15242edd1e9a63
MarBean <stevensav36@gmail.com>
2024-06-26 09:27:07 -0500

    chore: Update aiService.js to use correct file extension

--- routes/projectRoutes.cjs
+++ routes/projectRoutes.cjs
@@ -1,5 +1,5 @@
 const express = require('express');
-const generateCode = require('../services/aiService');
+const generateCode = require('../services/aiService.js');
 const router = express.Router();
 
 router.get('/projects/new', (req, res) => {
